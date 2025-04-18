9e76e3a39cd69dfcd6c5d1322dd5d5083017d829
Enes Tufekci <enesxtufekci@gmail.com>
2025-04-18 23:23:11 +0200

    init

---
+++ src/components/Header.tsx
@@ -0,0 +1,85 @@
+import { MenuIcon, XIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogHeader,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+  DialogPortal,
+} from "./ui/dialog";
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+  NavigationMenuTrigger,
+  NavigationMenuLink,
+} from "./ui/navigation-menu";
+
+const MobileMenu = () => {
+  return (
+    <Dialog>
+      <DialogTrigger className="lg:hidden">
+        <MenuIcon size="32" />
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogContent className="min-h-[400px]">
+          <DialogHeader className="hidden">
+            <DialogTitle>Mobile menu</DialogTitle>
+          </DialogHeader>
+          <div className="flex flex-col items-center justify-center h-full w-full">
+            <NavigationMenu>
+              <NavigationMenuList className="flex-col">
+                <NavigationMenuItem>
+                  <NavigationMenuLink>Docs</NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuLink>Pricing</NavigationMenuLink>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                  <NavigationMenuLink>Contact</NavigationMenuLink>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
+  );
+};
+
+export const Header = () => {
+  return (
+    <header className="container max-w-screen-xl mx-auto">
+      <nav className="flex items-center justify-between p-4">
+        <div>
+          <a href="/" className="flex items-center gap-2">
+            <img src="/logo.svg" alt="Logo" />
+            <span>LOGO</span>
+          </a>
+        </div>
+        <div className="hidden lg:flex gap-4">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink>Docs</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink>Pricing</NavigationMenuLink>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <NavigationMenuLink>Contact</NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+        <div>
+          <Button className="hidden lg:inline" variant="outline">
+            Sign in
+          </Button>
+          <MobileMenu />
+        </div>
+      </nav>
+    </header>
+  );
+};
